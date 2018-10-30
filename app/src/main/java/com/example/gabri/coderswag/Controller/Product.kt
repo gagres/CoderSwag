fa3c03c12cb08d8ca65fc2fbbb1574b03f3c3cca
Gabriel Giro Resende <gabriel.giro.resende@outlook.com>
2018-10-30 03:28:52 -0300

    Lesson 01

---
+++ app/src/main/java/com/example/gabri/coderswag/Controller/Product.kt
@@ -0,0 +1,3 @@
+package com.example.gabri.coderswag.Controller
+
+class Product constructor(val title: String, val price: String, val image: String)
---
+++ app/src/main/java/com/example/gabri/coderswag/Model/Category.kt
@@ -0,0 +1,3 @@
+package com.example.gabri.coderswag.Model
+
+class Category constructor(val title: String, val image: String)
